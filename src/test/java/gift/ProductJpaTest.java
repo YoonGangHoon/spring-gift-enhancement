fb708f7e2c57fab182f56f7be459161f47a60644
YoonGangHoon <yghun021007@naver.com>
2025-07-20 18:28:10 +0900

    refactor: 수정되지 않았던 페이징 테스트 로직 수정

--- src/test/java/gift/ProductJpaTest.java
+++ src/test/java/gift/ProductJpaTest.java
@@ -2,13 +2,14 @@ package gift;
 
 import gift.entity.Product;
 import gift.repository.ProductRepository;
-import gift.util.PagingUtils;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -66,7 +67,7 @@ public class ProductJpaTest {
         this.entityManager.persist(new Product("아인슈페너", 5500, "einspanner.jpg"));
 
         // when
-        Pageable pageable = PagingUtils.createPageable(1, 2, "id");
+        Pageable pageable = PageRequest.of(1, 2, Sort.by("id"));
         Page<Product> productsPage = productRepository.findAll(pageable);
 
         // then
